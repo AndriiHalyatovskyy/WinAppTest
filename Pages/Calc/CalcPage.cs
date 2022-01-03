b60130ee127bf46fb05e3a3c0097c170b17eb3a5
Andrii Halyatovskyy <andry1294@gmail.com>
2022-01-03 13:00:04 +0200

    Added calc page

---
+++ Pages/Calc/CalcPage.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace WinAppTest.Pages.Calc
+{
+    public class CalcPage : APage<CalcPageSelectors>
+    {
+        public CalcPage(Page p) : base(p, new CalcPageSelectors())
+        {
+        }
+    }
+
+    public class CalcPageSelectors
+    {
+
+    }
+}
--- Pages/Page.cs
+++ Pages/Page.cs
@@ -1,12 +1,27 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
+﻿using OpenQA.Selenium.Appium.Android;
+using OpenQA.Selenium.Support.UI;
+using System;
+using WinAppTest.Pages.Calc;
 
 namespace WinAppTest.Pages
 {
     public class Page
     {
+        private const int DEFAULT_WAIT = 20;
+        public AndroidDriver<AndroidElement> driver;
+        public WebDriverWait Wait;
+
+        private CalcPage calcPage;
+
+        public Page(AndroidDriver<AndroidElement> driver)
+        {
+            this.driver = driver;
+            Wait = new WebDriverWait(driver, TimeSpan.FromSeconds(DEFAULT_WAIT));
+        }
+
+        public CalcPage CalcPage
+        {
+            get { return calcPage ?? (calcPage = new CalcPage(this)); }
+        }
     }
 }
